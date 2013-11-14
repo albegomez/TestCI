0c9dbc03694d69e20bc672abd2609230b1628824
Alberto Gomez <alb.gomez@gmail.com>
2013-11-14 12:14:06 +0100

    Inicio de aplicacion

---
+++ TestCI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestCI.Startup))]
+namespace TestCI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
